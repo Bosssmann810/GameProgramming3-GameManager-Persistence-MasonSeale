5c3c1dab0e3f20ffd9e7a0573f4f7c6e563bd83e
Bosssmann810 <sealered@gmail.com>
2026-09-23 08:50:04 -0300

    the score xp and level can now be saved and loaded even between play sessions

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,5 +1,8 @@
 using UnityEngine;
 using System.Collections;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+using System;
 public class GameManager : MonoBehaviour
 {
     public float score;
@@ -28,9 +31,36 @@ public class GameManager : MonoBehaviour
 
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Save()
     {
-        Debug.Log(score);
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file = File.Create(Application.persistentDataPath + "/playerInfo.dat");
+        PlayerData data = new PlayerData();
+        data.score = score;
+        data.level = level;
+        data.xp = xp;
+        bf.Serialize(file, data);
+        file.Close();
     }
+    public void Load()
+    {
+        if (File.Exists(Application.persistentDataPath + "/playerInfo.dat"))
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.Open(Application.persistentDataPath + "/playerInfo.dat", FileMode.Open);
+            PlayerData data = (PlayerData)bf.Deserialize(file);
+            file.Close() ;
+            score = data.score;
+            level = data.level;
+            xp = data.xp;
+        }
+    }
+
+    [Serializable]
+    class PlayerData
+    {
+        public float score;
+        public float level;
+        public float xp;
+    }  
 }
--- Assets/ScoreChanger.cs
+++ Assets/ScoreChanger.cs
@@ -12,21 +12,29 @@ public class ScoreChanger : MonoBehaviour
         {
             GameManager.instance.score--;
         }
-        if (GUI.Button(new Rect(10, 120, 100, 30), "Add level"))
+        if (GUI.Button(new Rect(10, 130, 100, 30), "Add level"))
         {
             GameManager.instance.level++;
         }
-        if (GUI.Button(new Rect(120, 120, 100, 30), "Subtract level"))
+        if (GUI.Button(new Rect(120, 130, 100, 30), "Subtract level"))
         {
             GameManager.instance.level--;
         }
-        if (GUI.Button(new Rect(10, 140, 100, 30), "Add xp"))
+        if (GUI.Button(new Rect(10, 160, 100, 30), "Add xp"))
         {
             GameManager.instance.xp++;
         }
-        if (GUI.Button(new Rect(120, 140, 100, 30), "Subtract xp"))
+        if (GUI.Button(new Rect(120, 160, 100, 30), "Subtract xp"))
         {
             GameManager.instance.xp--;
+        }
+        if (GUI.Button(new Rect(250, 100, 100, 30), "save"))
+        {
+            GameManager.instance.Save();
+        }
+        if (GUI.Button(new Rect(350, 100, 100, 30), "load"))
+        {
+            GameManager.instance.Load();
         }
     }
 }
